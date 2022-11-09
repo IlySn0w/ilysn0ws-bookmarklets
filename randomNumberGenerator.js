d0e121d8961166efd6039d12ecbf039b4e115581
IlySn0w <116466904+IlySn0w@users.noreply.github.com>
2022-11-09 09:08:51 -0500

    Create randomNumberGenerator.js

---
+++ randomNumberGenerator.js
@@ -0,0 +1,9 @@
+javascript:
+
+var min = parseInt(prompt('Enter the miniumum number:'));
+var max = parseInt(prompt('Enter the maximum number:'));
+
+result = Math.floor(Math.random() * max) + min;
+
+alert('Your random number is ' + result);
+alert('Programmed by IlySn0w\nMy Github: https://github.com/IlySn0w/ilysn0ws-bookmarklets');
